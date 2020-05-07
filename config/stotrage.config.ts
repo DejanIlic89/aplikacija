6d627cd8b054b50a62e11995b472197f05bcc452
Dejan Ilic <theekey@gmail.com>
2020-05-08 01:09:43 +0200

    Implement a real mime type check and image resize mechanizm

--- config/stotrage.config.ts
+++ config/stotrage.config.ts
@@ -1,4 +1,6 @@
 export const StorageConfig = {
-    photoDestination: '../storage/photos',
+    photoDestination: '../storage/photos/',
     photoMaxFileSize: 1024 * 1024 * 3, // u bajtovima => 1024 * 1024 * 3 = 3MB (sto je zapravo i puno za jednu sliku, ali neka ostane)
+    photoThumbSize: { width: 120, height: 100 },
+    photoSmallSize: { width: 320, height: 240 },
 };
