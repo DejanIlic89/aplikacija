e37c3a5c34b3853247686089819b0e23ac5a3610
Dejan Ilic <theekey@gmail.com>
2020-05-07 21:33:12 +0200

    Implement the photo upload mechanism

---
+++ config/stotrage.config.ts
@@ -0,0 +1,4 @@
+export const StorageConfig = {
+    photoDestination: '../storage/photos',
+    photoMaxFileSize: 1024 * 1024 * 3, // u bajtovima => 1024 * 1024 * 3 = 3MB (sto je zapravo i puno za jednu sliku, ali neka ostane)
+};
