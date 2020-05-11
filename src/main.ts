9218218f483fca5ada6cb2efa417a6132324b705
Dejan Ilic <theekey@gmail.com>
2020-05-11 15:29:42 +0200

    Implement statically served photos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { StorageConfig } from 'config/stotrage.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(StorageConfig.photo.destination, {
+    prefix: StorageConfig.photo.urlPrefix,
+    maxAge: StorageConfig.photo.maxAge,
+    index: false // ne moze da se indeksira i da se listaju slike u resurima
+  })
   await app.listen(3000);
 }
 bootstrap();
